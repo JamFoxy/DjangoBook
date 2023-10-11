1ea8204a73515bffc96760a10fad1c92c59d4ed0
JamesFox <salimnogorbekov@yandex.ru>
2023-10-11 16:53:19 +0600

    lab 2

--- book/models.py
+++ book/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+    GENRE = (
+        ('Genre','genre'),
+        ('Fantasy', 'fantasy'),
+        ('Detective', 'detective')
+    )
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='')
     cost = models.IntegerField()
     created_date = models.DateTimeField(auto_created=True)
+    author = models.CharField(max_length=100, null=True)
+    genre = models.CharField(max_length=100, choices=GENRE, default=GENRE[0], null=True)
 
     def __str__(self):
         return self.title
